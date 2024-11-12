08da975e42f4c66b42a2575e7596d304921d2d8c
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-11-12 08:44:11 +0300

    Add files via upload

---
+++ 2.class_threading.py
@@ -0,0 +1,34 @@
+import threading
+import time
+
+class Knight(threading.Thread):
+
+    def __init__(self, name, power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        enemies = 100
+        days = 0
+        while enemies > 0:
+            days += 1
+            print(f'{self.name} сражается {days} день(дня)..., осталось {enemies} воинов.')
+            enemies -= self.power
+            time.sleep(1)
+        print(f'{self.name} одержал победу спустя {days} дней(дня)!')
+
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+if first_knight.is_alive() == False and second_knight.is_alive() == False:
+    print('Все битвы закончились!')
